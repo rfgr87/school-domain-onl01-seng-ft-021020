d8ebb3b7738a5820f17623ed5c32ab07be8feed0
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-19 19:02:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+
+class School
+  def initialize(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  ROSTER = {}
+
+  def add_student(student, grade)
+    ROSTER[grade] = []
+    ROSTER[grade] << student
+  end
+  
+  def roster
+    ROSTER
+  end
+  
+  def add_student(student, grade)
+    if ROSTER[grade] == nil
+    ROSTER[grade] = []
+    ROSTER[grade] << student
+  end
+end
